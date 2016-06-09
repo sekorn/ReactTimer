804911d49b52a02b67c334570991eec821034e10
Scott <sekorn@gmail.com>
2016-06-09 09:11:06 -0400

    initial commit

---
+++ app/components/nav.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer App</li>
+            <li>
+              <Link to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</Link>
+            </li>
+            <li>
+              <Link to="/countdown" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Created by <a href="http://www.rirruto.com" target="_blank">Scott Kornblatt</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Nav;
